86bde562b26822c43bb223ddefaac84be3245f69
XaeroVincent <tmp386@live.com>
2026-08-09 16:09:34 -0700

    Backport upstream driver from 16-CURRENT to 15.1

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # FreeBSD-NTSync
-FreeBSD 15+ NTSync kernel driver for use with Windows compatibility layers.
-
-**AI Disclosure:** _AI agents were used to assist with development._
+FreeBSD 15.1 NTSync kernel driver, backported from 16-CURRENT, for use with Windows compatibility layers.
 
 Additional NTSync test suite:
 https://github.com/monwarez/ntsync-test
--- build.sh
+++ build.sh
@@ -7,33 +7,4 @@ build_module() {
     sudo kldload ntsync
 }
 
-build_tests() {
-    cc -pthread -O2 -Wall -Wextra -I/usr/local/include ${PWD}/tests/ntsync_full_multithreaded_test.c -o ${PWD}/bin/ntsync_full_multithreaded_test
-    cc -O2 -Wall -Wextra -I/usr/local/include ${PWD}/tests/ntsync_create_wait.c -o ${PWD}/bin/ntsync_create_wait
-    cc -O2 -Wall -Wextra -I/usr/local/include ${PWD}/tests/ntsync_full_test.c -o ${PWD}/bin/ntsync_full_test
-    cc -O2 -Wall -Wextra -I/usr/local/include ${PWD}/tests/ntsync_test.c -o ${PWD}/bin/ntsync_test
-    cc -O2 -Wall -Wextra -I/usr/local/include ${PWD}/tests/ioctl_test.c -o ${PWD}/bin/ioctl_test
-}
-
-run_tests() {
-    ${PWD}/bin/ioctl_test
-    ${PWD}/bin/ntsync_create_wait
-    ${PWD}/bin/ntsync_test /dev/ntsync
-    ${PWD}/bin/ntsync_full_test /dev/ntsync
-    ${PWD}/bin/ntsync_full_multithreaded_test /dev/ntsync
-}
-
-run_debug() {
-    local child_pid="${1}"
-
-    WINEDEBUG=+sync,+server wine wineboot 2>&1 | head -200 &> winelog.txt
-    sudo ktrace -f ktrace.out ${PWD}/bin/ntsync_full_test
-    sudo kdump -f ktrace.out > kdump.txt
-    sudo procstat -k ${child_pid}
-    procstat -t ${child_pid}
-}
-
-#build_module
-#build_tests
-#run_tests
-#run_debug <child-pid-arg>
+build_module
